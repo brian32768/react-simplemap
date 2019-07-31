f43e1e37bbd8d2ddc8f9c19e3778ba05f3f27d80
Brian H Wilson <bwilson@co.clatsop.or.us>
2019-07-30 17:01:38 -0700

    left out the most important file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import MapPage from './map'
+import Map from './map'
 
 const App = () => {
   return (
     <div className="App">
-    <MapPage style={{position:'absolute',top:0,width:600,height:400,padding:20}}/>
+    <h1>react-simplemap</h1>
+    <Map style={{position:'relative',top:0,width:600,height:400,padding:10}}/>
+
+    <p>
+        Very simple <a href="https://reactjs.org">React</a> app to demonstrate a map
+        using <a href="https://openlayers.org">OpenLayers</a> and <a href="https://openstreetmap.org">OpenStreetMap</a>
+    </p><p>
+        Add it to your own project with <b>npm install @map46/react-simplemap</b>
+    </p>
     </div>
   );
 }
---
+++ src/map/index.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react'
+import PropTypes from 'prop-types'
+
+import olMap from 'ol/map'
+import View from 'ol/view'
+import {Tile} from 'ol/layer'
+import {OSM} from 'ol/source'
+
+const Map = (props) => {
+    const [theMap] = useState(new olMap({
+        view: new View({center: [0,0], zoom: 4}),
+        layers: [new Tile({source: new OSM()})]
+    }));
+    const mapTarget = element => {theMap.setTarget(element)}
+    return (
+        <div ref={mapTarget} style={props.style}>
+        </div>
+    );
+}
+export default Map
