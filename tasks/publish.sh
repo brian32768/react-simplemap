acebe82106faae69e5bbd523d809dc5b5333dff2
Brian H Wilson <bwilson@co.clatsop.or.us>
2019-08-01 16:56:19 -0700

    modify package.json when publishing

--- tasks/publish.sh
+++ tasks/publish.sh
@@ -73,6 +73,7 @@ main() {
   npm install
   npm run build-package
   cd ${BUILT_PACKAGE}
+  sed -i 's#../openlayers-6#../../../openlayers-6#' package.json
   pwd
   echo publishing...
   npm publish
