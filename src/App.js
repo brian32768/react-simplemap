049a515f12dd2f5524c9afbb13793b5ebb104a87
Brian H Wilson <bwilson@co.clatsop.or.us>
2019-08-12 17:00:34 -0700

    updates and added pointer watcher that makes rendering messed up hmmm

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React from 'react';
-import Map, {OpenLayersVersion} from './map'
+import React, {useState} from 'react';
+import {Map, OpenLayersVersion} from './map'
+import {toLonLat} from 'ol/proj'
 
 const App = () => {
-  return (
-    <div className="App">
-    <h1>react-simplemap</h1>
-    <h3><OpenLayersVersion/></h3>
-    <Map lat={45.5} lon={-122.6} zoom={10} style={{position:'relative',top:0,width:600,height:400,padding:10}}/>
+    const [pointer, setPointer] = useState();
 
-    <p>
-        Very simple <a href="https://reactjs.org">React</a> app to demonstrate a map
-        using <a href="https://openlayers.org">OpenLayers</a> and <a href="https://openstreetmap.org">OpenStreetMap</a>
-    </p><p>
-        Add it to your own project with <b>npm install @map46/react-simplemap</b>
-    </p>
-    </div>
-  );
+    const handlePointer = (e) => {
+        setPointer(toLonLat(e.coordinate));
+        e.stopPropagation();
+    }
+
+    const handleMove = (e) => {
+        console.log('move', e);
+        return false;
+    }
+
+    return (
+        <div className="App">
+            <h1>react-simplemap</h1>
+            {pointer}
+            <h3><OpenLayersVersion/></h3>
+            <Map center={[-122.6, 45.5]} zoom={10} onMoveEnd={handleMove} onPointerMove={handlePointer}
+                style={{position:'relative',top:0,width:600,height:400,padding:10}}/>
+
+                Simple <a href="https://reactjs.org">React</a> app to demonstrate a map
+                using <a href="https://openlayers.org">OpenLayers</a> and <a href="https://openstreetmap.org">OpenStreetMap</a>
+            <br />
+                Add it to your own project with <b>npm install @map46/react-simplemap</b>
+
+        </div>
+    );
 }
 
 export default App;
--- src/map/index.js
+++ src/map/index.js
@@ -1,31 +1,3 @@
-import React, {useState} from 'react'
-import PropTypes from 'prop-types'
-
-import olMap from 'ol/map'
-import {fromLonLat} from 'ol/proj'
-import View from 'ol/view'
-import {Tile} from 'ol/layer'
-import {OSM} from 'ol/source'
-
+export {default as Map} from './map'
+export {default as Version} from './version'
 export {default as OpenLayersVersion} from './ol-version'
-
-const Map = (props) => {
-    const [theMap] = useState(new olMap({
-        view: new View({
-            center: fromLonLat([props.lon, props.lat]),
-            zoom: props.zoom
-        }),
-        layers: [new Tile({source: new OSM()})]
-    }));
-    const mapTarget = element => {theMap.setTarget(element)}
-    return (
-        <>
-            <div ref={mapTarget} style={props.style}></div>
-        </>
-    );
-}
-Map.propTypes = {
-    center: PropTypes.arrayOf(PropTypes.number),
-    zoom: PropTypes.number
-}
-export default Map
